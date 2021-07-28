eb769fd67be6b2f3f53f8448533818c22721e03a
Anupam <anupam.mishra@gigatorb.com>
2021-07-28 12:21:19 +0530

    Products-check

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Products from "./Components/Products";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
